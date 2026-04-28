96191adb9f8f3b7965c3511cc873667da0480377
lucaspensotti <lucasleonel.pensotti@gmail.com>
2026-04-28 02:17:55 -0300

    feat: creé controllers.js, agregué funciones, exporté e importé en index.js

---
+++ controllers.js
@@ -0,0 +1,17 @@
+const getUsers = () => {
+    return "Obteniendo usuarios"
+}
+
+const createUser = () => {
+    return "Usuario creado"
+}
+
+const updateUser = () => {
+    return "Usuario actualizado"
+}
+
+const deleteUser = () => {
+    return "Usuario borrado"
+}
+
+export { getUsers, createUser, updateUser, deleteUser }
---
+++ index.js
@@ -0,0 +1,29 @@
+import { getUsers, createUser, updateUser, deleteUser } from "./controllers.js"
+
+const argv = process.argv
+const parameters = argv.slice(2)
+const operation = parameters[0]
+let resultado
+
+switch(operation) {
+    case "get":
+        resultado = getUsers()
+        break
+    case "add":
+        resultado = createUser()
+        break
+    case "update":
+        resultado = updateUser()
+        break
+    case "delete":
+        resultado = deleteUser()
+        break  
+    default:
+        resultado = "Operación inválida"
+}
+
+const main = () => {
+    console.log(resultado)
+}
+
+main()
